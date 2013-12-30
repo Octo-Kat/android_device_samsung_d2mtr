6c5f1458eaf13488c9080e9cb0700235a2407f05
Carl Windhausen <cjwindhausen@gmail.com>
2013-12-30 13:03:30 -0500

    Update and rename cm.mk to oct.mk

--- oct.mk
+++ oct.mk
@@ -12,6 +12,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="samsung/d2mtr/d2mtr:4.1.2/JZO54K/R530MVQAMF2:user/release-keys" \
     PRIVATE_BUILD_DESC="d2mtr-user 4.1.2 JZO54K R530MVQAMF2 release-keys"
 
-PRODUCT_NAME := cm_d2mtr
+PRODUCT_NAME := oct_d2mtr
 PRODUCT_DEVICE := d2mtr
 
