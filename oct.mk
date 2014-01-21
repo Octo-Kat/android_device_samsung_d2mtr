0b868569c28dc8506ad5c769174aa287cedfa720
Carl Windhausen <cjwindhausen@gmail.com>
2014-01-21 11:18:36 -0500

    Update oct.mk

--- oct.mk
+++ oct.mk
@@ -1,11 +1,5 @@
 $(call inherit-product, device/samsung/d2mtr/full_d2mtr.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2mtr \
     TARGET_DEVICE=d2mtr \
